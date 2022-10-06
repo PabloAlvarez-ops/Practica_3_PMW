188752f811600906820cd37710cdd3cca171f933
Pablo Álvarez <79628886+PabloAlvarez-ops@users.noreply.github.com>
2022-10-06 17:38:15 +0000

    Version que funciona

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
-function vaciar(){
-      docuemnt.getElementById("pantalla") = "";
+
+  var num, acc = 0, op = "";
+  function vaciar() {
+     document.getElementById("pantalla").value = "";
     }
 
-function cuadrado(){
-  let num = document.getElementById("pantalla");
-  num.value = num.value * num.value;
-}
-
-function mod(){
-  let num = document.getElementById("pantalla");
-  if(num.value >= 0){
-    num.value = num.value
-  } else{
-    num.value = num.value * -1
+  //Operaciones unitarias//
+  function cuadrado() {
+      let num = document.getElementById("pantalla");
+      num.value = num.value * num.value;
+    }
+    
+  function fact() {
+    let num = document.getElementById("pantalla");
+  if (num.value == 0 || num.value == 1) {
+    num.value = 1
+  }
+  else { 
+  for (var i = num.value - 1; i >= 1; i--) {
+    num.value *= i;
+  }
+  num.value
   }
 }
 
-function fact(){
-  let num = document.getElementById("pantalla");
-  if(num.value == 0 || num.value == 1){
-    return 1
-  } else{
-    for (var i=num.value-1; i>=1; i--){
-      num.value *= i
+  function mod() {
+    let num = document.getElementById("pantalla");
+    if (num.value < 0) {
+      num.value = -1*num.value
+    }
+    else {
+      num.value = num.value
     }
   }
-
   
-// function fact() {
-//   let num = document.getElementById("pantalla");
-//   if (num.value === 0 || num.value === 1)
-//     return 1;
-//   for (var i = num.value - 1; i >= 1; i--) {
-//     num.value *= i;
-//   }
-//   return num.value;
-// }
+  //Operaciones binarias//
+  function add() {
+       acc = num.value;  op = "+acc++num.value";
+    }
+
+  function eq() {
+    console.log(op)
+    num.value = eval(op) 
+  }
+  
