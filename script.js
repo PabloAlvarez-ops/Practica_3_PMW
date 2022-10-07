d3f2e05439db042f6429c1afacc69e6787d9d277
Pablo Álvarez <79628886+PabloAlvarez-ops@users.noreply.github.com>
2022-10-07 16:54:00 +0000

    terminada, excepto bloque validacion

--- script.js
+++ script.js
@@ -18,24 +18,25 @@ const rellenar_info = () => {
 // 2. Operaciones unitarias //
 
 function cuadrado() {
-  let num = document.getElementById("pantalla");
-  num.value = num.value * num.value;
+  let num = document.getElementById("pantalla"); // Obtenemos el input introducido por el usuario
+  num.value = num.value * num.value; // Calculamos el cuadrado: numero multiplicado por si mismo.
   rellenar_info();
 }
 
 function mod() {
-  let num = document.getElementById("pantalla");
-  if (num.value < 0) {
+  let num = document.getElementById("pantalla"); // Obtenemos el input introducido por el usuario
+  if (num.value < 0) { // Para cualquier numero negativo, lo multiplicamos por -1 para obtenerlo en +-
     num.value = -1 * num.value
   }
   else {
-    num.value = num.value
+    num.value = num.value // Si ya esta en + se deja como esta. Asi obtenemos el valor absoluto.
   }
   rellenar_info();
 }
 
 function fact() {
-  let num = document.getElementById("pantalla");
+  let num = document.getElementById("pantalla"); // Obtenemos el input introducido por el usuario
+  // 0! y 1! dan como resultado 1. En caso contrario (else) debemos multiplicarlo por su anterior hasta llegar a 1.
   if (num.value == 0 || num.value == 1) {
     num.value = 1
   }
@@ -83,49 +84,49 @@ const guardaInput = (numInput) => {
 
 }
 
-const mul = () => {
-  guardaInput(Number(document.getElementById("pantalla").value));
-  total = document.getElementById("pantalla").value;
-  operador = "multiplicar";
+function mul() {
+  guardaInput(Number(document.getElementById("pantalla").value)); //Guardamos el primer numero introducido 
+  total = document.getElementById("pantalla").value; //Guardamos el segundo numero introducido
+  operador = "multiplicar"; //Una vez tenemos el operador a aplicar vamos al 'case' correspondiente.
   vaciar();
 }
 
-const add = () => {
+function add() {
   guardaInput(Number(document.getElementById("pantalla").value));
   total = Number(document.getElementById("pantalla").value);
   operador = "sumar";
   vaciar();
 }
 
-const rest = () => {
+function rest() {
   guardaInput(Number(document.getElementById("pantalla").value));
   total = document.getElementById("pantalla").value;
   operador = "restar";
   vaciar();
 }
 
-const div = () => {
+function div() {
   guardaInput(Number(document.getElementById("pantalla").value));
   total = document.getElementById("pantalla").value;
   operador = "dividir";
   vaciar();
 }
 
-const res = () => {
+function res() {
   guardaInput(Number(document.getElementById("pantalla").value));
   total = document.getElementById("pantalla").value;
   operador = "resto";
   vaciar();
 }
 
-const pot = () => {
+function pot() {
   guardaInput(Number(document.getElementById("pantalla").value));
   total = document.getElementById("pantalla").value;
   operador = "potencia";
   vaciar();
 }
 
-const eq = () => {
+function eq() { //Al pulsar el boton de igual, en funcion del operador introducido aplicará una operacion u otra.
   guardaInput(Number(document.getElementById("pantalla").value));
   operador = "";
   if (operador === "multiplicar") {
@@ -141,7 +142,7 @@ const eq = () => {
   } else if (operador === "potencia") {
     num.value **= total
   }
-  document.getElementById("pantalla").value = total;
+  document.getElementById("pantalla").value = total; // Muestra el resultado de la operación.
   rellenar_info();
 }
 
@@ -150,9 +151,10 @@ const eq = () => {
 // 4. Operaciones en formato CSV //
 
 function sumatorio() {
-  let num = document.getElementById("pantalla");
-  let list = num.value.split(",");
+  let num = document.getElementById("pantalla"); //Obtenemos el input introducido
+  let list = num.value.split(","); // Separamos el input por comas y el resultado lo almacena en una lista
 
+  // Sumamos los elementos contenidos en la lista
   let i = 0, acc = 0;
   while (i < list.length) acc += +list[i++];
  
@@ -161,6 +163,37 @@ function sumatorio() {
   
 }
 
+const sortNumbers = (a, b) => {
+      if (a > b) {
+        return 1;
+      } else if (b > a) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
 
+  function ordenar() {
+    let listaString = document.getElementById("pantalla").value; //Obtenemos el input introducido
+    let arrayNumeros = listaString.split(","); // Separamos el input por comas y el resultado lo almacena en una lista
+    document.getElementById("pantalla").value = arrayNumeros.sort(sortNumbers); // modificamos el input para que muestre la lista de numeros ordenada de forma ascendente.
+    rellenar_info();
+  }
+
+function revertir() {
+    let listaString = document.getElementById("pantalla").value; //Obtenemos el input introducido
+    let arrayNumeros = listaString.split(","); // Separamos el input por comas y el resultado lo almacena en una lista
+    document.getElementById("pantalla").value = arrayNumeros.reverse(sortNumbers); // modificamos el input para que muestre la lista de numeros ordenada de forma descendente.
+    rellenar_info();
+  }
+
+function quitar() {
+      let listaString = document.getElementById("pantalla").value; //Obtenemos el input introducido
+      let arrayNumeros = listaString.split(","); // Separamos el input por comas y el resultado lo almacena en una lista
+      arrayNumeros.pop(); // eliminamos el ultimo elemento de la lista
+      document.getElementById("pantalla").value = arrayNumeros; // modificamos el input para que muestre la lista de numeros con el ultimo eliminado.
+      rellenar_info();
+
+    }
 
 
